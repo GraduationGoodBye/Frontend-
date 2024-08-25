1013379f61c3b5bbe5f6a7ee34a3648c4b24670e
youseock <mahwin7085@gmail.com>
2024-08-25 15:18:47 +0900

    :bento:feat: 폰트, 색상 관련 작업
    - 폰트 에셋 저장
    - 색상, 폰트 tailwind.config 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Home() {
-  return <div>123</div>;
+  return (
+    <>
+      <div className="text-background_grey">123</div>
+      <div className="text-primary">123</div>
+      <div className="text-secondary">123</div>
+      <div className="text-background_light font-super">123</div>
+    </>
+  );
 }
