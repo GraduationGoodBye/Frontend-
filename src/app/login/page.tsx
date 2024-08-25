9e739496e4ab2920691c56a5a61d9a49f5f0d8a2
jypark <asa855@nate.com>
2024-08-25 21:24:04 +0900

    :sparkles:feat : 로그인 페이지 UI 작업
    * 소셜 로그인 브랜드 로고 및 로그인 이미지 추가
    * 소셜 로그인 컴포넌트 분리

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { Flex } from "../../shared/ui";
+import Image from "next/image";
+import { Icon } from "../../shared/icon/icon";
+import { Metadata } from "next";
+import LoginButtons from "./LoginButtons";
+
+export const metadata: Metadata = {
+  title: "Login",
+  description: "GGB login page",
+};
 
 const page = () => {
   return (
     <div className="flex h-screen w-full">
-      <div className="flex w-full flex-col items-center justify-center bg-background_light">
+      <Flex.ColCenter className="bg-background_light">
         <p className="text-center font-super text-4xl">Graduation, Good bye.</p>
         <p className="text-center font-super text-4xl">
           and Say Hi to NEW GENERATIONS
         </p>
-      </div>
-      <Flex.ColCenter className="bg-primary">
+      </Flex.ColCenter>
+      <Flex.ColCenter className="gap-8 bg-primary">
         <p className="font-super text-[32px]">sign in!</p>
-        <button>google</button>
+        <LoginButtons />
       </Flex.ColCenter>
     </div>
   );
--- src/shared/icon/icon.tsx
+++ src/shared/icon/icon.tsx
@@ -109,4 +109,47 @@ function Default({ width = 151, height = 126 }: Props) {
   );
 }
 
-export const Icon = { Wink, LookUp, LookDown, Default };
+function NaverLogo({ width = 21, height = 20 }: Props) {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 21 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14.173 10.693L6.66913 0.0999756H0.452148V19.9H6.96996V9.30698L14.4571 19.9H20.6741V0.0999756H14.173V10.693Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
+
+function KakaoLogo({ width = 36, height = 36 }: Props) {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 36 36"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_1_126)">
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M17.9999 1.2C8.05823 1.2 -0.00012207 7.42591 -0.00012207 15.1046C-0.00012207 19.88 3.11669 24.0899 7.86293 26.5939L5.86593 33.889C5.6895 34.5336 6.42671 35.0474 6.99281 34.6738L15.7466 28.8964C16.4853 28.9677 17.236 29.0093 17.9999 29.0093C27.9408 29.0093 35.9997 22.7836 35.9997 15.1046C35.9997 7.42591 27.9408 1.2 17.9999 1.2Z"
+          fill="black"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_1_126">
+          <rect width="35.9999" height="36" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export const Icon = { Wink, LookUp, LookDown, Default, NaverLogo, KakaoLogo };
