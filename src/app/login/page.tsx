babc14950000c0707fead83fe069e590d4856285
jypark <asa855@nate.com>
2024-08-31 17:05:13 +0900

    :sparkle:feat : google login 초안 작업

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,11 @@
 import React from "react";
-import { Flex } from "../../shared/ui";
 import { Metadata } from "next";
-import LoginButtons from "../../features/login/ui/LoginButtons";
 import { Login } from "../../widgets";
 
 export const metadata: Metadata = {
   title: "Login",
   description: "GGB login page",
 };
-
 const page = () => {
   return <Login />;
 };
